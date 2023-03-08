f34b7251a6a99354928154578d49498c9d88e987
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-03-08 19:59:09 +0300

    Who likes it

---
+++ Who likes it.js
@@ -0,0 +1,14 @@
+function likes(names) {
+    if (names.length == 1) return `${names[0]} likes this`
+    else if (names.length == 2) return `${names[0]} and ${names[1]} like this`
+    else if (names.length == 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    else if (names.length >= 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    
+    return 'no one likes this'
+  }
+
+console.log(likes([]));
+console.log(likes(['Peter']));
+console.log(likes(['Jacob', 'Alex']));
+console.log(likes(['Max', 'John', 'Mark']));
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
